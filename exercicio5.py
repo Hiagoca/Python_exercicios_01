c925a6629e42497f2e1f7598010780d631a9abaa
Hiagoca <hiagoca.senac@gmail.com>
2024-07-10 14:00:52 -0300

    EXE

---
+++ exercicio5.py
@@ -0,0 +1,4 @@
+var_int = int(input("Digite um número inteiro: "))
+var_float = float(input("Digite um número decimal: "))
+var_palavra = str(input("Digite uma palavra: "))
+print(f"As informações digitadas foram: {var_int}, {var_float}, {var_palavra}.")
---
+++ exercicio6.py
@@ -0,0 +1,4 @@
+var_1 = float(input("Digite um número: "))
+var_2 = float(input("Digite um segundo número:"))
+var_3 = float(input("Digite um terceiro número: "))
+print(f"A média entre os números fornecidos é {(var_1+var_2+var_3)/3:.2f}.")
