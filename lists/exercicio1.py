59492a69a1bdb38df83c40349bcd3d717173e7f0
Hiagoca <hiagoca.senac@gmail.com>
2024-07-15 16:52:15 -0300

    Create exercicio1.py

---
+++ lists/exercicio1.py
@@ -0,0 +1,7 @@
+minha_lista = ["laranja", "morango", "pera", "uva"]
+print(minha_lista[1])
+minha_lista.append ("jaca")
+print(minha_lista)
+print(len(minha_lista))
+minha_lista.remove("uva")
+print(minha_lista)
